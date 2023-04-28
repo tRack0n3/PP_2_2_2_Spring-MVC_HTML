c97a13618e9f1e9e013348eb19c74188c635c3e8
Vladislav Nalobin <110870376+tRack0n3@users.noreply.github.com>
2023-04-28 15:22:06 +0300

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,12 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController {
